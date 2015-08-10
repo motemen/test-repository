6e529617bf32b7dda4b0bb3ab9ae8bfd7dd96367
motemen <motemen@gmail.com>
2015-08-10 17:43:42 +0900

    vars

--- scripts/bump-release.sh
+++ scripts/bump-release.sh
@@ -4,17 +4,22 @@ set -e
 
 pr_number=$(git show --pretty=%s | sed -n 's/^Merge pull request #\([0-9]\{1,\}\) .*/\1/p')
 
+repo_owner=motemen
+repo_name=test-repository
+committer_email=motemen+gobump-bot@gmail.com
+committer_name=motemen
+
 if [ -n "$pr_number" ]; then
   new_version=$(gobump patch -w -v | jq -r '.[]')
 
   if ! git diff --exit-code ./*.go; then
-    git config --global user.email 'motemen+gobump-bot@gmail.com'
-    git config --global user.name  'motemen'
+    git config --global user.email "$committer_email"
+    git config --global user.name  "$committer_name"
 
     git add ./*.go
     git commit -m "bump version to $new_version"
-    git push "https://$GITHUB_TOKEN@github.com/motemen/test-repository" HEAD:master >/dev/null 2>&1
+    git push "https://$GITHUB_TOKEN@github.com/$repo_owner/$repo_name" HEAD:master >/dev/null 2>&1
 
-    ghr --token "$GITHUB_TOKEN" --replace --draft "v$new_version" dist/
+    ghr --user "$repo_owner" --token "$GITHUB_TOKEN" --replace --draft "v$new_version" dist/
   fi
 fi
