ebfcc504890d054f32d22769059f0ee1cfe38058
motemen <motemen@gmail.com>
2015-08-10 13:37:34 +0900

    tra

---
+++ scripts/bump-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pr_number=$(git show --pretty=%s | sed -n 's/^Merge pull request #\([0-9]\{1,\}\) .*/\1/p')
+
+if [ -n "$pr_number" ]; then
+    new_version=$(gobump $command -w -v | jq -r '.[]')
+
+  if ! git diff --exit-code ./*.go; then
+    git add ./*.go
+    git commit -m "bump version to $new_version"
+    git push "https://$GITHUB_TOKEN@github.com/motemen/test-repository" HEAD:master
+
+    export GOBUMP_NEW_VERSION="$new_version"
+  fi
+fi
