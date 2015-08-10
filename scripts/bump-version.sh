7d5857bd0d197c51330d6d06ab232847da39538e
motemen <motemen@gmail.com>
2015-08-10 13:42:09 +0900

    bump

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 pr_number=$(git show --pretty=%s | sed -n 's/^Merge pull request #\([0-9]\{1,\}\) .*/\1/p')
 
 if [ -n "$pr_number" ]; then
-    new_version=$(gobump $command -w -v | jq -r '.[]')
+    new_version=$(gobump patch -w -v | jq -r '.[]')
 
   if ! git diff --exit-code ./*.go; then
     git add ./*.go
